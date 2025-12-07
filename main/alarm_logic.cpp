52ba53b844e8f3c982ce3ed5957efea83d40125f
Chirita Marius <chiritamarius818@gmail.com>
2025-12-07 13:04:51 +0100

    alarm files and additional comments on implementation

---
+++ main/alarm_logic.cpp
@@ -0,0 +1,13 @@
+//  The alarm system will take input parameters the values from gas sensors and for each values it should check if the values is above the limit. If it is, send a 10V signal to the PLC
+//  and if not, exit the method, basicall return nothing and do nothing.
+
+// void checkLimitForAlarm(float gas_value)
+//       if (gas_value > limit)
+//       {      
+//          send_signal_to_PLC() //another function, specifically for sending the signal
+//       } 
+//
+
+// There should be another function for sending signal to the PLC: send_signal_to_PLC()
+
+// This is my train though and how I think the implementation should be, but of course, it remains to the choice of the programmer of this part.
--- main/main.ino
+++ main/main.ino
@@ -12,16 +12,40 @@ PWMmodule pwm(25, 0, 5000, 10);
 void setup() {
   Serial.begin(115200)
   Wire.begin(21, 22) // SDA -> 21, SCL -> 22
-  ads1015.begin();
+  ads1015.begin(); // External ADC setup
   pwm.begin()
   // put your setup code here, to run once:
-
 }
 
 void loop() {
+   
+   ////////// Sensor reading logic + PWM
+   //
+   // The temperature sensors va will go on every 20s (not sure, could be changed), the gas sensors will go on immediately one after another
+   //
+   /////////
+
+  // ADC logic - external ADC will convert the NDIR, sensor 2, temperature sensor 1, internal ADC will convert the last two temperature sensors
   //if(NDIR & S2 & T1)
     digital_value = ads1015.readADC_SingleEnded(0);
   //else
     float digital_value = readADC(adc_pin);
 
+  /// I2C Logic
+  //
+  // Digital values from the external ADC will go through the I2C for PLC and OLED implementation
+  //
+  ////// 
+
+  //// PLC alarm Logic
+  //
+  // Input parameters: gas digital values, Output parameters: 10V signal to the PLC
+  //
+  ///////
+
+  ///// OLED logic
+  //
+  // Input parameters: gas digital values, Output: numbers on screen
+  //
+  /////
 }
